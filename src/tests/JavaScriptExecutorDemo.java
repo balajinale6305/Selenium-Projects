225081c5dbbf6b58861b5b2168b88586d7d30092
Balaji Nale <balajinale6305@gmail.com>
2025-09-22 00:49:12 +0530

    new update

--- src/tests/JavaScriptExecutorDemo.java
+++ src/tests/JavaScriptExecutorDemo.java
@@ -1,7 +1,11 @@
 package tests;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaScriptExecutorDemo {
@@ -12,12 +16,18 @@ public class JavaScriptExecutorDemo {
 		driver.manage().window().maximize();
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("window.scrollBy(0, 500)");
-		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
-		driver.close();
+		//Sum of elements present in the table in the webpage
+		List<WebElement>values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+		int sum = 0;
+		for(int i=0; i<values.size(); i++) {
+			sum = sum + Integer.parseInt(values.get(i).getText());
+			 
+		}
 		
+		int total = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
 		
-		
+		assert sum == total;
 		
 
 	}
