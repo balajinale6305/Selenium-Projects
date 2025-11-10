22e42902e8909cefb740e82b73b3bf9d61c91368
Balaji Nale <balajinale6305@gmail.com>
2025-11-10 22:35:22 +0530

    latest commit

--- src/main/NewWindow.java
+++ src/main/NewWindow.java
@@ -1,20 +1,26 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/angularpractice/");
 		driver.manage().window().maximize();
+		//Switching Window
 		driver.switchTo().newWindow(WindowType.TAB);
 		Set<String> handles = driver.getWindowHandles();
 		Iterator<String> it = handles.iterator();
@@ -22,12 +28,20 @@ public class NewWindow {
 		String childWindow = it.next();
 		driver.switchTo().window(childWindow);
 		driver.get("https://rahulshettyacademy.com");
-		String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
+		String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']"))
+				.get(1).getText();
 		driver.switchTo().window(parentWindow);
-		driver.findElement(By.name("name")).sendKeys(courseName);
-		driver.quit();
+		WebElement name = driver.findElement(By.name("name"));
+		name.sendKeys(courseName);
+		//Screenshot
+		File file = name.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("logo.png"));
+
+		// Get Height and Width
 		
-		
+		System.out.println(name.getRect().getDimension().getHeight());
+		System.out.println(name.getRect().getDimension().getWidth());
+		driver.quit(); 
 
 	}
 
