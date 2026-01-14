01d99de783184dd6c4f7a3bc7415c526b98b2a51
Balaji Nale <balajinale6305@gmail.com>
2026-01-15 01:25:27 +0530

    complete POM

---
+++ eclipse-workspace/javawithselenium/src/test/java/seleniumpractice/javawithselenium/StandAloneTest.java
@@ -0,0 +1,38 @@
+package seleniumpractice.javawithselenium;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class StandAloneTest {
+
+	static String productName = "ZARA COAT 3";
+	static String countryName = "india";
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.goTo();
+		ProductCatalogue productCatalogue = landingPage.loginApplication("balajinale1997@gmail.com", "Balaji@6305");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay(productName);
+		Assert.assertTrue(match);
+		CheckoutPage checkoutPage = cartPage.goToCheckout();
+		checkoutPage.selectCountry(countryName);
+		ConfirmationPage confirmationPage = checkoutPage.submitOrder();
+		String confirmMessage = confirmationPage.getConfirmationMessage();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER"));
+		driver.close();
+
+	}
+
+}
